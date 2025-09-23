601641d3454c3fa97f3fc593147ad90a5a58e8e8
Harsh Patidar <harshpatidar242004@gmail.com>
2025-09-23 16:17:56 +0530

    replaces the hosted url in frontend

--- test/src/api/axios.ts
+++ test/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import { getAllUsers } from "../../../Server/Controller/userController";
 
-const API_BASE_URL = "http://localhost:4000";
+const API_BASE_URL = "https://usertable-1.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
