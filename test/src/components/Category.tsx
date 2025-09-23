99c3f89fe18ae059565d0c71852ebdbc8edea910
Harsh Patidar <harshpatidar242004@gmail.com>
2025-09-23 18:38:51 +0530

    upfdate

--- test/src/components/Category.tsx
+++ test/src/components/Category.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Category({ age }: { age: number }) {
   if (age < 18) return <span className="category-child">child</span>;
   else if (age >= 18 && age <= 60) return <span className="category-young">young</span>;
-  else if (age > 150) return <span className="  ">budhe sale nikal yaha se</span>;
+  // else if (age > 150) return <span className="  ">budhe sale nikal yaha se</span>;
   return <span className="category-elder">elder</span>;
 }
 
